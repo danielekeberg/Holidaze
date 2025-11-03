dfb9a0db78c10b78ea5632be399893b39c6763cf
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-11-03 16:09:24 +0100

    Create Register/page.tsx

---
+++ app/register/page.tsx
@@ -0,0 +1,24 @@
+import Register from "@/app/components/Register";
+
+export default function Home() {
+  return (
+    <>
+      <div className="mx-[15%]">
+        <div className="my-5 flex justify-center">
+                <div className="w-125">
+                    <div className="flex justify-center mb-10">
+                        <img src="/logo.png" className="h-10"/>
+                    </div>
+                    <div className="mb-5">
+                        <h1 className="text-2xl">Sign in</h1>
+                        <p>Sign in</p>
+                    </div>
+                    <div>
+                        <Register />
+                    </div>
+                </div>
+            </div>
+      </div>
+    </>
+  );
+}
