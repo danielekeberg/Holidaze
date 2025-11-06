62d7331964ed7b0ff101b9e901d6bc2a5ec1cf73
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-06 16:48:23 +0100

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,4 +1,13 @@
+'use client';
+import { useState } from "react";
+
 export default function Hero() {
+    const [s, setS] = useState("");
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        window.location.href = `./properties/${s}`;
+    }
     return(
         <div className="relative h-[600px] flex items-center jusitfy-center overflow-hidden">
             <div className="absolute inset-0 z-0"
@@ -18,40 +27,19 @@ export default function Hero() {
                     <h1 className="text-6xl text-white font-extrabold text-center drop-shadow-[0_2px_6px_rgba(0,0,0,0.65)] mb-3">Find your next adventure</h1>
                     <h1 className="text-2xl text-white text-center drop-shadow-[0_2px_6px_rgba(0,0,0,0.85)]">Discover amazing places to stay around the world</h1>
                 </div>
-                <div className="bg-white flex gap-5 p-5 rounded-xl border border-neutral-800/30 justify-center items-center">
-                    <div className="border-r border-neutral-800/30 px-2">
+                <form className="bg-white flex gap-5 p-5 rounded-xl border border-neutral-800/30 justify-center items-center" onSubmit={handleSubmit}>
+                    <div className="border-r border-neutral-800/30 px-2 w-100">
                         <p className="text-sm mb-2 mx-1 text-neutral-600 font-bold">Location</p>
                         <div className="flex gap-2 items-center">
                             <img src="/location.svg" className="h-5" />
-                            <input type="text" placeholder="Where are you going?" className="outline-none" />
+                            <input type="text" placeholder="Where are you going?" onChange={(e) => setS(e.target.value)} className="outline-none w-full" />
                         </div>
                     </div>
-                    <div className="border-r border-neutral-800/30 px-2">
-                        <p className="text-sm mb-2 mx-1 text-neutral-600 font-bold">Check in</p>
-                        <div className="flex gap-2 items-center">
-                            <img src="/calendar.svg" className="h-5" />
-                            <input type="date" placeholder="Where are you going?" />
-                        </div>
-                    </div>
-                    <div className="border-r border-neutral-800/30 px-2">
-                        <p className="text-sm mb-2 mx-1 text-neutral-600 font-bold">Check out</p>
-                        <div className="flex gap-2 items-center">
-                            <img src="/calendar.svg" className="h-5" />
-                            <input type="date" placeholder="Where are you going?" />
-                        </div>
-                    </div>
-                    <div>
-                        <p className="text-sm mb-2 mx-1 text-neutral-600 font-bold">Guests</p>
-                        <div className="flex gap-2 items-center">
-                            <img src="/users.svg" className="h-5" />
-                            <input type="number" placeholder="2" className="outline-none" />
-                        </div>
-                    </div>
-                    <button className="flex gap-2 bg-blue-500 px-4 py-2 rounded-xl text-white">
+                    <button type="submit" className="flex gap-2 bg-blue-500 px-4 py-2 rounded-xl text-white cursor-pointer">
                         <img src="/search.svg" className="h-5" />
                         <p className="text-sm font-bold">Search</p>
                     </button>
-                </div>
+                </form>
             </div>
             
         </div>
