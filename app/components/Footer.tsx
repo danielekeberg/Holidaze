e7a754c6bfc63644c51a343ca4a5ce2df0624b47
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-06 16:48:09 +0100

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const now = new Date();
     const year = now.getFullYear();
     return(
-        <div className="border-t border-neutral-400 p-10 text-center">
+        <div className="border-t border-neutral-400 p-10 mt-20 text-center">
             <p>&copy; Holidaze {year}</p>
         </div>
     )
