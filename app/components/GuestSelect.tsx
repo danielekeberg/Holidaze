db7b3b2325ce1d4d961e91cadd3746eac105d3f7
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-06 16:48:16 +0100

    Craete GuestSelect.tsx

---
+++ app/components/GuestSelect.tsx
@@ -0,0 +1,11 @@
+export default function GuestSelect({ maxGuests }) {
+    return (
+        <>
+        {Array.from({ length: maxGuests }, (_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {i + 1} {i + 1 === 1 ? "guest" : "guests"}
+          </option>
+        ))}
+        </>
+    )
+}
