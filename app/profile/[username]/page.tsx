adf3ba5166f86d72065bb0f05c231ce0b3b19d2e
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-13 14:46:46 +0100

    Updage profile/page.tsx

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/app/components/Header";
 import Dashboard from "@/app/components/Dashboard";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex flex-col mx-5 md:mx-70 h-[80vh]">
+      <div className="flex flex-col mx-5 md:mx-70 mb-100">
         <Dashboard />
       </div>
     </>
