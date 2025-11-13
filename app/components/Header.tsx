b8db74069c3302bc6660d95a53b2db91783022dc
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-13 14:45:58 +0100

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -92,7 +92,7 @@ export default function Header() {
                                             <p className="cursor-pointer hover:bg-neutral-200 py-2 px-5 border-b border-neutral-300">Contact Us</p>
                                         </Link>
                                         <Link href="../login/">
-                                            <p className="cursor-pointer w-full hover:bg-neutral-200 py-2 px-5 border-b border-neutral-300">Login</p>
+                                            <p className="cursor-pointer w-full hover:bg-neutral-200 py-2 px-5 border-b border-neutral-300">Sign in</p>
                                         </Link>
                                     </>
                                 }
