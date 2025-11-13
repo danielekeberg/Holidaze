646ecdef010946bdbf106d93b163b153115f5d20
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-13 14:46:29 +0100

    Update Properties.tsx

--- app/components/Properties.tsx
+++ app/components/Properties.tsx
@@ -34,29 +34,22 @@ type Venue = {
     }
 }
 
-const PAGE_SIZE = 20;
-
 export default function Properties() {
     const [loading, setLoading] = useState(true);
     const [venues, setVenues] = useState<Venue[]>([]);
     const [sortBy, setSortBy] = useState("popular");
     const [err, setErr] = useState<any>();
     const [query, setQuery] = useState("");
-    const [page, setPage] = useState(0);
     const params = useParams();
     const { search } = params;
     const [hasMore, setHasMore] = useState(false);
+    const [noVenues, setNoVenues] = useState(false);
 
     useEffect(() => {
         setLoading(true);
         async function fetchVenues() {
             try {
-                const res = await fetch(`${search === "displayall" ? 'https://v2.api.noroff.dev/holidaze/venues/' : `https://v2.api.noroff.dev/holidaze/venues/search?q=${search}`}`, {
-                    headers: {
-                        "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGFuaWVsZWtlYmVyZyIsImVtYWlsIjoiZGFuZWtlNzUxNjBAc3R1ZC5ub3JvZmYubm8iLCJpYXQiOjE3NjE1OTc2ODN9.OKeXS9-bygAHKddfK5XQyjhjhjeouMqDjTFH3fgsMcU`,
-                        "X-Noroff-API-Key": "d937e2e1-dc41-4685-9054-22798ad19d5e",
-                    }
-                });
+                const res = await fetch(`${search === "displayall" ? 'https://v2.api.noroff.dev/holidaze/venues/' : `https://v2.api.noroff.dev/holidaze/venues/search?q=${search}`}`);
                 const data = await res.json();
                 const venues = data.data;
                 setVenues(venues);
@@ -64,6 +57,9 @@ export default function Properties() {
                     setHasMore(false);
                 }
                 setLoading(false);
+                if(data.data.length < 1) {
+                    setNoVenues(true);
+                }
                 console.log(data);
             } catch(err) {
                 setErr(err);
@@ -71,7 +67,7 @@ export default function Properties() {
         }
         fetchVenues();
     }, []);
-
+    
     const displayedVenues = useMemo(() => {
         const q = query.trim().toLowerCase();
         const filtered = q
@@ -105,7 +101,8 @@ export default function Properties() {
                     <h1 className="font-bold text-3xl mt-10">Explore Properties</h1>
                     <p className="text-neutral-600">Find your perfect vacation rental</p>
                 </div>
-                <div className="flex gap-10 justify-end">
+                <div className="flex flex-col md:flex-row gap-5 justify-between">
+                    <input type="text" placeholder="Fungerer ikke enda :(" className="p-2 border border-neutral-300" />
                     <select 
                         id="popular"
                         value={sortBy}
@@ -123,35 +120,40 @@ export default function Properties() {
                         <Spinner />
                     </div>
                     :
-                    ''
+                    null
                 }
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-                    {displayedVenues.map((v, i) => (
+                    {!noVenues ?
+                        displayedVenues.map((v, i) => (
                         <Link href={`../venue/${v.id}`} key={i} className="rounded-md overflow-hidden shadow group">
-                            <div>
-                                <img src={v.media[0]?.url} className="h-50 w-full object-cover transition duration-300 group-hover:scale-105" />
-                            </div>
-                            <div className="p-4">
                                 <div>
-                                    <div className="flex items-center justify-between">
-                                        <h5 className="w-2/3 font-bold">{v.name}</h5>
-                                        <div className="flex items-center gap-1">
-                                            <img src="/star.svg" className="h-5" />
-                                            <p className="font-bold">{v.rating}</p>
+                                    <img src={v.media[0]?.url} className="h-50 w-full object-cover transition duration-300 group-hover:scale-105" />
+                                </div>
+                                <div className="p-4">
+                                    <div>
+                                        <div className="flex items-center justify-between">
+                                            <h5 className="w-2/3 font-bold">{v.name}</h5>
+                                            <div className="flex items-center gap-1">
+                                                <img src="/star.svg" className="h-5" />
+                                                <p className="font-bold">{v.rating}</p>
+                                            </div>
+                                        </div>
+                                        <div className="flex gap-1 items-center mt-2">
+                                            <img src="/location.svg" className="h-5" />
+                                            <p className="text-neutral-600">{v.location.city ? v.location.city : 'City'}, {v.location.country ? v.location.country : 'Country'}</p>
                                         </div>
                                     </div>
-                                    <div className="flex gap-1 items-center mt-2">
-                                        <img src="/location.svg" className="h-5" />
-                                        <p className="text-neutral-600">{v.location.city ? v.location.city : 'City'}, {v.location.country ? v.location.country : 'Country'}</p>
+                                    <div className="flex gap-2 items-center mt-2">
+                                        <h1 className="text-xl font-bold">${v.price}</h1>
+                                        <span className="text-neutral-600">/ night</span>
                                     </div>
                                 </div>
-                                <div className="flex gap-2 items-center mt-2">
-                                    <h1 className="text-xl font-bold">${v.price}</h1>
-                                    <span className="text-neutral-600">/ night</span>
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
+                            </Link>
+                        ))
+                    :
+                        'No venues found.'
+                    }
+                    
                 </div>
                 {hasMore && (
                     <div className="flex justify-center mb-10">
